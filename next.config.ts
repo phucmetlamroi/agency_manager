2ba7ca3980289771de2f9428393bc0b70222abcc
Agency Admin <admin@agency.com>
2026-02-03 16:02:53 +0700

    Config: Allow Vercel Blob hostname in next/image

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+images: {
+  remotePatterns: [
+    {
+      protocol: 'https',
+      hostname: 'public.blob.vercel-storage.com',
+    },
+    {
+      protocol: 'https',
+      hostname: '*.public.blob.vercel-storage.com',
+    }
+  ],
+  },
 };
 
 export default nextConfig;
